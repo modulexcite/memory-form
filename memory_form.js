8f0244949af1caa1f43cf5bbbe922d76cc3eb17a
Stephen Korecky <skorecky@gmail.com>
2012-12-09 17:06:51 -0600

    cleaning up js, adding min js

--- memory_form.js
+++ memory_form.js
@@ -10,15 +10,15 @@
         if (sessionStorage[inputName] !== undefined && inputs[i].name !== "") {
           inputs[i].value = sessionStorage[inputName];
         }
-      };
-      document.onkeyup = function(event){
+      }
+      document.onkeyup = function(){
         for (var i = inputs.length - 1; i >= 0; i--){
           inputName = inputs[i].name;
           if (inputs[i].name !== "") {
             sessionStorage[inputName] = inputs[i].value;
           }
-        };
+        }
       };
-    };
-  }
+    }
+  };
 })();
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # Memory Form
 
+## About
+
 Too often users are filling out a form or survey and the accidently reload the page and lose all their hard work filling out that form. No more!
 
 It works by saving everytime a users inputs data to a session in local storage. This way the data will be removed as soon as the browser is restarted.
+
+## Usage
+
+Simply download the script and include it in your html view file. Ideally in the footer is best, but whereever is fine.
